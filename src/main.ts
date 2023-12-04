95d4a8242e07918d16b29baf40af84bc72ced783
Nicholas Licata <nicholas.licata730@gmail.com>
2023-12-04 00:01:54 -0500

    added all quiz functionality

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,65 @@
-import './style.css'
+import './style.css';
+import allQuestions from './questions.ts';
 
+const quizContainer = document.querySelector('.quiz-container') as HTMLDivElement;
+const questionDisplay = document.querySelector('.question-display') as HTMLParagraphElement;
+const choicesDisplay = document.querySelector('.choices-display') as HTMLDivElement;
+const startButton = document.querySelector('.start-btn') as HTMLButtonElement;
+const nextButton = document.querySelector('.next-btn') as HTMLButtonElement;
+let quizIndex = 0;
+let score = 0;
+
+startButton?.addEventListener('click', startQuiz);
+nextButton?.addEventListener('click', nextQuestion);
+
+function startQuiz() {
+  startButton.style.visibility = 'hidden';
+  displayQuiz();
+}
+ 
+function displayQuiz() {
+  if (quizIndex <= 9) {
+  const choices = allQuestions[quizIndex].choices;
+  choices.forEach((choice) => {
+    const choiceButton = document.createElement('button');
+    choiceButton.textContent = String(choice.choice);
+    choicesDisplay.append(choiceButton);
+    choiceButton.addEventListener('click', makeChoice);
+    questionDisplay.textContent = allQuestions[quizIndex].question;
+  })
+} else if (quizIndex > 9) {
+  questionDisplay.style.visibility = 'hidden';
+  choicesDisplay.style.visibility = 'hidden';
+  nextButton.style.visibility = 'hidden';
+  displayScore();
+}
+}
+
+function nextQuestion() {
+ quizIndex++
+ clearPreviousQuestion();
+ displayQuiz();
+}
+
+function clearPreviousQuestion() {
+  while (choicesDisplay.hasChildNodes()) {
+    choicesDisplay.childNodes.forEach(previous => {
+      previous.remove();
+    })
+  }
+}
+
+function makeChoice(e) {
+  const userChoice = e.currentTarget;
+  const choice = Array.from(choicesDisplay.children);
+  const correctChoice = allQuestions[quizIndex].correctAnswer;
+    if (choice.indexOf(userChoice) === correctChoice) {
+      score++
+    }
+}
+
+function displayScore() {
+  const scoreDisplay = document.createElement('span');
+  quizContainer.appendChild(scoreDisplay);
+  scoreDisplay.textContent = `You scored ${String(score)} out of 10!`;
+}
