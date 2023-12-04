1d21bf8c13f21c01473ab9e2f86243dfd49d4121
Nicholas Licata <nicholas.licata730@gmail.com>
2023-12-04 00:02:18 -0500

    uploaded quiz data file

---
+++ src/questions.ts
@@ -0,0 +1,44 @@
+
+const allQuestions = [
+    {question: 'What is a baby kangaroo called?',
+     choices: [{choice: 'Donkey'}, {choice: 'Gary'}, {choice: 'Joey'}, {choice:'Ricky'}],
+     correctAnswer: 2},
+
+    {question: 'What is the closest planet to the Sun?',
+     choices: [{choice:'Mercury'}, {choice: 'Earth'}, {choice: 'Jupiter'}, {choice: 'Uranus'}],
+     correctAnswer: 0},
+    
+    {question: 'How many letters are in the English alphabet?',
+     choices: [{choice: '20'}, {choice: '22'},{choice: '24'}, {choice: '26'}],
+     correctAnswer: 3},
+
+    {question: 'What is the name of the tallest mountain on Earth?',
+     choices: [{choice: 'Mt.Everest'}, {choice: 'Mt.Fuji'}, {choice: 'Mt.Kilimanjaro'}, {choice: 'Mt.Olympus'}],
+     correctAnswer: 0},
+
+    {question: 'How many months have 31 days?',
+     choices: [{choice: '8'}, {choice: '6'}, {choice: '9'}, {choice: '7'}],
+     correctAnswer: 3},
+
+    {question: 'Where do polar bears live?',
+     choices: [{choice: 'Poland'}, {choice: 'The Arctic'}, {choice: 'Polar Planet'}, {choice: 'America'}],
+     correctAnswer: 1},
+
+    {question: 'How many continents are there?',
+     choices: [{choice: '5'}, {choice: '10'}, {choice: '8'},{choice: '7'}],
+     correctAnswer: 3},
+
+    {question: 'How many days are in a year?',
+     choices: [{choice: '355'}, {choice: '365'}, {choice: '375'}, {choice: '380'}],
+     correctAnswer: 1},
+
+    {question: 'What is the name of a shape with 5 sides?',
+     choices: [{choice: 'Polygon'}, {choice: 'Decagon'}, {choice: 'Octagon'}, {choice: 'Pentagon'}],
+     correctAnswer: 3},
+
+    {question: 'In which country can you find the Eiffel Tower?',
+     choices: [{choice: 'Germany'}, {choice: 'Finland'}, {choice: 'France'}, {choice: 'Canada'}],
+     correctAnswer: 2}
+]
+
+export default allQuestions;
